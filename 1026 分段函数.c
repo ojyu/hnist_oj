d997f5dc9bdd9d98c47aee7c4f720470c89c6540
y <oj_hnist_acm@yeah.net>
2023-04-24 19:12:33 +0800

    1026

---
+++ 1026 分段函数.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int x;
+	scanf("%d", &x);
+	if (x < 0)
+		printf("%d", x * x - 1);
+	else if (x >= 1)
+		printf("%d", x * x + 1);
+	else
+		printf("%d", x * x);
+}
