e4743dc0d22d01d21e937e725d7b97aa006b5905
y <oj_hnist_acm@yeah.net>
2023-05-01 23:22:56 +0800

    1075

---
+++ 1075 表重复元素删除.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int n = 0;
+	int i = 0;
+	int j = 0;
+	scanf("%d", &n);
+	int* p = (int*)calloc(n, sizeof(int));
+	for (i = 0; i < n; i++)
+		scanf("%d", &p[i]);
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = i + 1; j < n; j++)
+		{
+			if (p[i] == p[j])
+			{
+				int temp = 0;
+				for (temp = j; temp < n - 1; temp++)
+					p[temp] = p[temp + 1];
+				j--;
+				n--;
+			}
+		}
+	}
+	printf("%d\n", n);
+	for (i = 0; i < n; i++)
+		printf("%d ", p[i]);
+	return 0;
+}
