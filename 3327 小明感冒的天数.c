372e38096b1fc69c5a6bd8d2d6d54a0ca15b597b
y <oj_hnist_acm@yeah.net>
2023-06-05 14:29:38 +0800

    3327

---
+++ 3327 小明感冒的天数.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int n, m;
+	int i;
+	int day = 0;
+	int count = 0;
+	scanf("%d%d", &n, &m);
+	int* p = calloc(n, sizeof(int));
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", p + i);
+		if (day < p[i])
+		{
+			day = p[i] + m - 1;
+			count += m;
+		}
+	}
+	printf("%d", count);
+	return 0;
+}
